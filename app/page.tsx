91961a54083383475e1cab855fa6cc438602c6b2
dosazuwa <dsazuwa@gmail.com>
2024-05-25 18:17:14 +0100

    add glass effect to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/footer';
 import Blog from '@/components/home/blog';
 import Brands from '@/components/home/brands';
 import CTA from '@/components/home/cta';
@@ -8,17 +9,21 @@ import Reviews from '@/components/home/reviews';
 
 export default function Home() {
   return (
-    <main className='flex w-full flex-col gap-[10vh] pb-[10vh] pt-5'>
-      <div className='space-y-10'>
-        <Hero />
-        <Brands />
-      </div>
+    <>
+      <main className='flex w-full flex-col gap-[10vh] pb-[10vh]'>
+        <div className='space-y-6'>
+          <Hero />
+          <Brands />
+        </div>
 
-      <Overview />
-      <Blog />
-      <Reviews />
-      <FAQ />
-      <CTA />
-    </main>
+        <Overview />
+        <Blog />
+        <Reviews />
+        <FAQ />
+        <CTA />
+      </main>
+
+      <Footer />
+    </>
   );
 }
