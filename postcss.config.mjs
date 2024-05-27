e7980e2a254b596512b32e56450220794c5500a8
dosazuwa <dsazuwa@gmail.com>
2024-05-27 17:51:54 +0100

    optimize tailwind for production

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('postcss-load-config').Config} */
+
+const cssnano = [
+  'cssnano',
+  {
+    preset: 'advanced',
+    discardComments: { removeAll: true },
+  },
+];
+
 const config = {
   plugins: {
     tailwindcss: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
 
